acfa31631d567efcf5b2019fdb43f2ef900e0865
aheig <61814388+aheig@users.noreply.github.com>
2020-10-19 10:57:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git litte https://github.com/kenzok8/litte package/litte' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git litte https://github.com/kenzok8/litte package/litte' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small feeds.conf.default
